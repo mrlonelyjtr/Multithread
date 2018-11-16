1c109e25c3c5966e775cb3b17eedc04410cfa3b5
Ma Tianyao <mrlonelyjtr@icloud.com>
2018-11-17 00:30:34 +0800

    guarded suspension ex3

---
+++ Guarded Suspension/3/ServerThread.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class ServerThread extends Thread {
+    private final RequestQueue requestQueue;
+    private final Random random;
+
+    public ServerThread(RequestQueue requestQueue, String name, long seed) {
+        super(name);
+        this.requestQueue = requestQueue;
+        this.random = new Random(seed);
+    }
+
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            Request request = requestQueue.getRequest();
+            System.out.println(Thread.currentThread().getName() + " handles " + request);
+            
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}

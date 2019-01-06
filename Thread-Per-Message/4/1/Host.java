fb7e0a2d2b870722942cb93a3e863a0fa4bd6acc
Ma Tianyao <mrlonelyjtr@icloud.com>
2019-01-06 23:21:57 +0800

    ex 2 & 4

---
+++ Thread-Per-Message/4/1/Host.java
@@ -0,0 +1,9 @@
+public class Host {
+    private final Helper helper = new Helper();
+
+    public void request(int count, char c) {
+        System.out.println("    request(" + count + ", " + c + ") BEGIN");
+        new HelperThread(helper, count, c).start();
+        System.out.println("    request(" + count + ", " + c + ") END");
+    }
+}
